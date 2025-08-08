fbbc09b3441905569f3c9953bdaad3de39d41ff7
vladmikhbond <vladmikhbond@gmail.com>
2025-08-08 13:34:02 +0000

    .

--- test.py
+++ test.py
@@ -1,17 +1,135 @@
-import re
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+import jwt
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jwt.exceptions import InvalidTokenError
+from passlib.context import CryptContext
+from pydantic import BaseModel
 
-# Вхідні дані
-x = """
-Код до 
-//BEGIN 
-тимчасовий код 
-//END 
-код після"""
-regex = "//BEGIN.*//END"
-y = "[ЗАМІНЕНО]"
+# -------------- Дані, спільні для А і С  --------------------
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-# Заміна першого фрагмента, який відповідає regex
-result = re.sub(regex, y, x, count=1, flags=re.DOTALL)
 
-# Виведення результату
-print("Результат:", result)
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$wXQX36VoV9NhLUVRS9gDPuliZGbKTg/p3mxjXEnSO6smBwKa4Nwje",  # for pass = '123456'
+    }
+}
+
+# -------------- Моделі Pydantic--------------------
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+
+class UserInDB(User):
+    hashed_password: str
+
+
+# -------------- Виключно для хешування паролів --------------------
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+# -------------- Юзер менеджер --------------------
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+# -------------- Безпекова залежність --------------------
+
+# Витягує токен із запиту. Перевіряє лише наявність токену, а не його зміст.
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    """
+    Розшифровує токен, використовуючі SECRET_KEY, ALGORITHM.
+    За даними з токену знаходить користувача в БД і повертає його.
+    """
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+# -------------- Створення токену --------------------
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    """
+    Створює зашифрований токен, використовуючі
+    SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+    """
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+# ----------------- Кінцева точка, яка видає токени ---------------------------
+
+app = FastAPI()
+
+@app.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token = create_access_token(
+        data={"sub": user.username}, 
+        expires_delta=timedelta(minutes=15)  # видихнеться за 15 хвилин
+    )
+    return Token(access_token=access_token, token_type="bearer")
